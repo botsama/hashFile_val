799e35312bed4599fe7c34483adddd2b3f5df597
Pic0o <47042955+botsama@users.noreply.github.com>
2019-06-10 11:51:41 -0400

    Update getHashFile.ps1

--- getHashFile.ps1
+++ getHashFile.ps1
@@ -1,6 +1,6 @@
 # calc hash in sha256 for file in defined output location
 
-$searchPath = 'C:\ScriptSearch\debug_War'
+$searchPath = 'C:\ScriptSearch\path'
 $outHashFileName = '.hash'
 $buildSearch = Get-ChildItem $($searchPath) -Recurse -Force -Include *.war, *.ear, *.rar, *.zip, *.xml -ErrorAction SilentlyContinue
 # Search defined path for packages and zip archives.
